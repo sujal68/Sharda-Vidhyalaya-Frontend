f8305b4cffdf50ae41337201eabbdecf3a5ff372
sujal68 <sujalkidecha68@gmail.com>
2025-11-20 12:46:39 +0530

    sharda

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5000';
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'https://sharda-vidhyalaya.vercel.app/';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
